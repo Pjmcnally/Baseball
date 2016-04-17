d04e12dc9d2d8765621382cc0b1d66fec9474de6
Pjmcnally <Patrick.J.McNally@gmail.com>
2016-04-16 23:54:59 -0700

    Progress establishing a many to many field.  However, does not seem to have helped much with load times.  Not sure how to proceed

--- baseball_proj/atbatvis/views.py
+++ baseball_proj/atbatvis/views.py
@@ -9,9 +9,10 @@ def index(request):
 def test(request):
     player = Player.objects.get(id='mauej001')
 
-    plays = player.play.select_related("game").all()
+    games = player.game_set.all()
+    
     # for play in plays:
     #     games.get(play.game, []).append(play)
     # print(games)
-    context = {"player": player, "plays": plays}
+    context = {"player": player, "games": games}
     return render(request, "atbatvis/test.html", context)
