ac828f249ff08e62cff0c08a5888df686698266f
Pjmcnally <Patrick.J.McNally@gmail.com>
2016-04-16 22:19:02 -0700

    Flailing to fix database query issue

--- baseball_proj/atbatvis/views.py
+++ baseball_proj/atbatvis/views.py
@@ -9,10 +9,9 @@ def index(request):
 def test(request):
     player = Player.objects.get(id='mauej001')
 
-    games = {}
-    plays = player.play.all()
-    for play in plays:
-        games.get(play.game, []).append(play)
-    print(games)
-    context = {"player": player, "games": games}
+    plays = player.play.select_related("game").all()
+    # for play in plays:
+    #     games.get(play.game, []).append(play)
+    # print(games)
+    context = {"player": player, "plays": plays}
     return render(request, "atbatvis/test.html", context)
